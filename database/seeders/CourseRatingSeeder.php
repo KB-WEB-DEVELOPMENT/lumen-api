d8461c2a83122b54ff2c67fce8766ed573ff9112
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-09-16 08:48:03 +0200

    Update CourseRatingSeeder.php

--- database/seeders/CourseRatingSeeder.php
+++ database/seeders/CourseRatingSeeder.php
@@ -14,21 +14,21 @@ class CourseRatingSeeder extends Seeder
     public function run(): void
     {
         $course1_id = Course::select('id')->where('title','Lumen Framework - Basics')->firstOrFail();
-		$course2_id = Course::select('id')->where('title','Lumen Framework - Advanced')->firstOrFail();
+	$course2_id = Course::select('id')->where('title','Lumen Framework - Advanced')->firstOrFail();
 		
-		$student1_id = Student::select('id')->where('lastname','Student1 lastname')->firstOrFail();
-		$student2_id = Student::select('id')->where('lastname','Student2 lastname')->firstOrFail();
+	$student1_id = Student::select('id')->where('lastname','Student1 lastname')->firstOrFail();
+	$student2_id = Student::select('id')->where('lastname','Student2 lastname')->firstOrFail();
 				
-		CourseRating::factory()->create([
+	CourseRating::factory()->create([
 			'rating' => 3,
 			'course_id' => $course1_id,
 			'student_id' => $student1_id,
-		]);
+	]);
 
-		CourseRating::factory()->create([
+	CourseRating::factory()->create([
 			'rating' => 5,
 			'course_id' => $course2_id,
 			'student_id' => $student2_id,
-		]);		
-	}
+	]);		
+    }
 }
