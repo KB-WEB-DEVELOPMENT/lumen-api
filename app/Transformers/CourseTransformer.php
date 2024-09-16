f29a1994ecd494377d5a99b6915cda756be263ec
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-09-16 08:34:59 +0200

    Update CourseTransformer.php

--- app/Transformers/CourseTransformer.php
+++ app/Transformers/CourseTransformer.php
@@ -16,15 +16,15 @@ class CourseTransformer extends TransformerAbstract
      */
     public function transform(Course $course)
     {
-		return [
+	return [
             'id' => $course->id,
             'title' => $course->title,
             'topic' => $course->topic, 
             'start_date' => date("DD/MM/YY",strtotime($course->start_date)), // 30/06/2025 
             'total_number_hours' => $course->total_number_hours, 
             'instructor_name' => $course->instructor->title . ' ' .  ucfirst(strtolower(trim($course->instructor->lastname))),
-			      'created' => $course->created_at,
-			      'updated' => $course->updated_at
-		];
+	    'created' => $course->created_at,
+	    'updated' => $course->updated_at
+	];
     }
 }
