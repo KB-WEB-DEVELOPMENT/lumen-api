e7123220142c3f49784668e2db7960caa2dd632e
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-09-16 08:48:32 +0200

    Update CourseSeeder.php

--- database/seeders/CourseSeeder.php
+++ database/seeders/CourseSeeder.php
@@ -13,21 +13,20 @@ class CourseSeeder extends Seeder
     {
         $instructor1_id = Instructor::select('id')->where('lastname','Instructor1 lastname')->firstOrFail();
 		
-		Course::factory()->create([
+	Course::factory()->create([
 			'title' => 'Lumen Framework - Basics',
 			'topic' =>  'PHP',
 			'start_date' => '2024-011-02T09:15:00+0000',
 			'total_number_hours' => 55,
 			'instructor_id' => $instructor1_id
-		]);
+	]);
 
-		Course::factory()->create([
+	Course::factory()->create([
 			'title' => 'Lumen Framework - Advanced',
 			'topic' =>  'PHP',
 			'start_date' => '2025-011-02T09:15:00+0000',
 			'total_number_hours' => 70,
 			'instructor_id' => $instructor1_id
-		]);
-		
-	}
+	]);		
+   }
 }
