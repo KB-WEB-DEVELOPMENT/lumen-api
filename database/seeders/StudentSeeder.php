a621562ab0f5e5a3a1add996817760e260625f34
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-09-16 08:50:19 +0200

    Update StudentSeeder.php

--- database/seeders/StudentSeeder.php
+++ database/seeders/StudentSeeder.php
@@ -12,18 +12,18 @@ class StudentSeeder extends Seeder
     public function run(): void
     {
         $user3_id = User::select('id')->where('name','name-user3-student1')->firstOrFail();
-		$user4_id = User::select('id')->where('name','name-user4-student2')->firstOrFail();
+	$user4_id = User::select('id')->where('name','name-user4-student2')->firstOrFail();
 		
-		Student::factory()->create([
+	Student::factory()->create([
 			'firstname' => 'Student1 firstname',
 			'lastname' =>  'Student1 lastname',
 			'user_id' => $user3_id,
-		]);
+	]);
 		
-		Student::factory()->create([
+	Student::factory()->create([
 			'firstname' => 'Student2 firstname',
 			'lastname' =>  'Student2 lastname',
 			'user_id' => $user4_id
-		]);		
-	}
+	]);		
+     }
 }
