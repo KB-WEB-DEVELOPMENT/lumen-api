f69ebdf3ec48b1156690d1a3c7fb1c4d1e4db340
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-09-16 08:50:37 +0200

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -11,8 +11,8 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         User::factory()->create(['name' => 'name-user1-instructor1']);
-		User::factory()->create(['name' => 'name-user2-instructor2']);
-		User::factory()->create(['name' => 'name-user3-student1']);
-		User::factory()->create(['name' => 'name-user4-student2']);
+	User::factory()->create(['name' => 'name-user2-instructor2']);
+	User::factory()->create(['name' => 'name-user3-student1']);
+	User::factory()->create(['name' => 'name-user4-student2']);
     }
 }
