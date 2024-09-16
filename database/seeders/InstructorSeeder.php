f5655d680564ca7c8b43cf38260ab9ff299b2cd7
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-09-16 08:49:49 +0200

    Update InstructorSeeder.php

--- database/seeders/InstructorSeeder.php
+++ database/seeders/InstructorSeeder.php
@@ -11,21 +11,21 @@ class InstructorSeeder extends Seeder
 {
     public function run(): void
     {
-		$user1_id = User::select('id')->where('name','name-user1-instructor1')->firstOrFail();
-		$user2_id = User::select('id')->where('name','name-user2-instructor2')->firstOrFail();
+	$user1_id = User::select('id')->where('name','name-user1-instructor1')->firstOrFail();
+	$user2_id = User::select('id')->where('name','name-user2-instructor2')->firstOrFail();
 		
-		Instructor::factory()->create([
-		    'title' => 'Professor',
-			'firstname' => 'Instructor1 firstname',
-			'lastname' =>  'Instructor1 lastname',
-			'user_id' => $user1_id,
-		]);
+	Instructor::factory()->create([
+	    'title' => 'Professor',
+	    'firstname' => 'Instructor1 firstname',
+	    'lastname' =>  'Instructor1 lastname',
+	    'user_id' => $user1_id,
+	]);
 		
-		Instructor::factory()->create([
-		    'title' => 'Professor',
-			'firstname' => 'Instructor2 firstname',
-			'lastname' =>  'Instructor2 lastname',
-			'user_id' => $user2_id
-		]);		
-	}
+	Instructor::factory()->create([
+	    'title' => 'Professor',
+	    'firstname' => 'Instructor2 firstname',
+	    'lastname' =>  'Instructor2 lastname',
+	     'user_id' => $user2_id
+	]);		
+     }
 }
