08093cdce01480b53e7c2116940689a136be5152
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-09-16 08:35:53 +0200

    Update InstructorStatsTransformer.php

--- app/Transformers/InstructorStatsTransformer.php
+++ app/Transformers/InstructorStatsTransformer.php
@@ -21,27 +21,26 @@ class InstructorStatsTransformer extends TransformerAbstract
     public function transform(Instructor $instructor)
     {
 		
-		$registered_courses_num  = Course::where('instructor_id',$instructor->id)->count();
+	$registered_courses_num  = Course::where('instructor_id',$instructor->id)->count();
 		
-		$instructor_courses_ids  = Course::select('id')->where('instructor_id',$instructor->id)->get();
+	$instructor_courses_ids  = Course::select('id')->where('instructor_id',$instructor->id)->get();
 		
-		$courseRatingCollection =  CourseRating::all();
+	$courseRatingCollection =  CourseRating::all();
 		
-		$filteredRatings = $courseRatingCollection->whereIn('course_id',$instructor_courses_ids)->all();
+	$filteredRatings = $courseRatingCollection->whereIn('course_id',$instructor_courses_ids)->all();
  
-		$num_students_votes = $filteredRatings->count();
+	$num_students_votes = $filteredRatings->count();
  
-		$average_stars_rating =  $num_students_votes == 0 ? 'unavailable' : number_format($filteredRatings->sum('rating')/$num_students_votes,1,'.','');                                                   
+	$average_stars_rating =  $num_students_votes == 0 ? 'unavailable' : number_format($filteredRatings->sum('rating')/$num_students_votes,1,'.','');                                                   
 		
-		$average_percent_rating = $num_students_votes == 0 ? 'unavailable' : round(20*$average_stars_rating);  
+	$average_percent_rating = $num_students_votes == 0 ? 'unavailable' : round(20*$average_stars_rating);  
  		
-		return [
-		    'name' => $course->instructor->title . ' ' .  ucfirst(strtolower($course->instructor->lastname)),
-			'registered_courses' => $registered_courses_num,
-			'average_stars_rating' => ($average_stars_rating == 'unavailable') ? 'unavailable' : $average_stars_rating . '/5', 
+	return [
+	    'name' => $course->instructor->title . ' ' .  ucfirst(strtolower($course->instructor->lastname)),
+	    'registered_courses' => $registered_courses_num,
+	    'average_stars_rating' => ($average_stars_rating == 'unavailable') ? 'unavailable' : $average_stars_rating . '/5', 
             'average_percent_rating' => ($average_percent_rating == 'unavailable') ? 'unavailable' :  $average_percent_rating . '%',
-			'students_votes' => $num_students_votes,
+	    'students_votes' => $num_students_votes,
         ];
     }
 }
-
