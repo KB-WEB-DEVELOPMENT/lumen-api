ed26907b35fed0bb1c969ba8ce9a3d310c2dd087
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-09-16 07:23:12 +0200

    Create Student.php

---
+++ app/Student.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Student extends User
+{
+    protected $table = 'students';
+	/**
+     * The attributes that are mass assignable
+     *
+     * @var array
+     */
+    protected $fillable = ['firstname', 'lastname','user_id'];
+	
+	public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class,'id');
+    }
+}
